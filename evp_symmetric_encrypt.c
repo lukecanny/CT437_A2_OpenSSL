4ecf9bfb92d07ba3a2f32008ccb302ba3d8dfd28
lukecanny <l.canny3@universityofgalway.ie>
2024-02-22 12:53:04 +0000

    updated gitignore, added posix time recording

--- evp_symmetric_encrypt.c
+++ evp_symmetric_encrypt.c
@@ -10,13 +10,6 @@
 
 int main (void)
 {
-    /*
-     * Set up the key and iv. Do I need to say to not hard code these in a
-     * real application? :-)
-     */
-
-    /* A 256 bit key */
-    unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
 
     // unsigned char *key   - Define a "pointer" called key of type "unsigned char" 
     // (unsigned char *)    - Type cast the literal string into unsigned char pointer
@@ -26,6 +19,9 @@ int main (void)
         // the string "01234567890123456789012345678901" is obviously much bigger
         // What does unsigned char * as a type cast mean
 
+    /* A 256 bit key */
+    unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
+
     /* A 128 bit IV */
     unsigned char *iv = (unsigned char *)"0123456789012345";
 
@@ -33,41 +29,34 @@ int main (void)
     unsigned char *plaintext =
         (unsigned char *)"The quick brown fox jumps over the lazy dog";
 
-    /*
-     * Buffer for ciphertext. Ensure the buffer is long enough for the
-     * ciphertext which may be longer than the plaintext, depending on the
-     * algorithm and mode.
-     */
+    /* Buffer for ciphertext. */
     unsigned char ciphertext[128];
 
     /* Buffer for the decrypted text */
     unsigned char decryptedtext[128];
 
+    /* Plaintext / Ciphertext Length */
     int decryptedtext_len, ciphertext_len;
 
+    /* Struct for Time Recordings */
+    struct timespec en_time_start, en_time_end, de_time_start, de_time_end;
+
+    // Start recording encryption time:
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &en_time_start);
     /* Encrypt the plaintext */
     ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
                               ciphertext);
-    /*
-        encrypt returns length,
-        accepts following param:
-            plaintext: variable of plaintext value (plaintext)
-            plaintext length: strlen ( (char *) plaintext )     // What the hell is the type cast for
-            key: just passing the key
-            iv: just passing the iv
-            cipertext: just passing the variable (?? not the address to same memory space??)
-    
-    */
+    // Finish recording encryption time:
 
+    // /* Do something useful with the ciphertext here */
+    // printf("Ciphertext is:\n");
+    // BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
 
-
-    /* Do something useful with the ciphertext here */
-    printf("Ciphertext is:\n");
-    BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
-
+    // Begin recording decryption time:
     /* Decrypt the ciphertext */
     decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
                                 decryptedtext);
+    // Finish recording decryption time:
 
     /* Add a NULL terminator. We are expecting printable text */
     decryptedtext[decryptedtext_len] = '\0';
@@ -75,7 +64,6 @@ int main (void)
     /* Show the decrypted text */
     printf("Decrypted text is:\n");
     printf("%s\n", decryptedtext);
-
 
     return 0;
 }
